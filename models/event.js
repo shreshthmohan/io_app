b03a54b1c67cc19dbea1abc31f0940f6a34e0eed
Shreshth <sremog@gmail.com>
2014-11-21 17:41:06 +0530

    No functional change

--- models/event.js
+++ models/event.js
@@ -7,9 +7,13 @@ module.exports = function(sequelize, DataTypes) {
     organiser_url:   {type: DataTypes.STRING(255)},
     address_field:   {type: DataTypes.STRING(255)},
     location_url:    {type: DataTypes.STRING(255)},
-    start_date:      {type: DataTypes.DATE},
+    start_date:      {type: DataTypes.DATE}, // This is MySQL DATETIME 
     end_date:        {type: DataTypes.DATE},
     // ^ http://dev.mysql.com/doc/refman/5.5/en/datetime.html
+    // Since sequelize not support the creation of column with MySQL DATE
+    // we manually have to alter table after creation of database like so,
+    // "alter table Events modify column start_date date;"
+    // "alter table Events modify column end_date date;"
     comments:        {type: DataTypes.STRING(255)}
   },
   {
