a0dc7fc0db4e17cb01e0ef08697c452d4db21850
Shreshth <sremog@gmail.com>
2014-12-24 10:28:58 +0530

    Added City name in upcoming events view

--- routes/user/event.js
+++ routes/user/event.js
@@ -4,7 +4,6 @@ var sequelize = db.sequelize; // just to avoid the confusion
 
 // all upcoming events
 exports.upcoming = function(req,res) {
-  sequelize.query("select id, event_name, event_url, organiser_name, organiser_url, address_field, location_url, start_date as start_date_orig, date_format(start_date, '%d %M %Y') start_date, date_format(end_date, '%d %M %Y') end_date, comments, CityId from Events where start_date > NOW() order by start_date_orig", null, {raw: true} )
   sequelize.query("select Events.id as id, event_name, start_date as start_date_orig, date_format(start_date, '%d %M %Y') start_date, date_format(end_date, '%d %M %Y') end_date, comments, CityId, Cities.city_name as city_name, Cities.id as CityIdC from Events left outer join (select id, city_name from Cities) Cities on Events.CityId = Cities.id where start_date > NOW() order by start_date_orig", null, {raw: true} )
   // join to find city name
   .success(function(races) {
