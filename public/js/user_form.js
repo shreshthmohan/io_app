fd411d5f33b329aa3e2b60e1097fb4c190aeaaf7
Ubuntu <ubuntu@ip-172-31-8-38.ap-southeast-1.compute.internal>
2015-03-18 18:38:22 +0000

    Fixed user "Tip Us" javascript

--- public/js/user_form.js
+++ public/js/user_form.js
@@ -1,1 +1,25 @@
-$(document).ready(function(){$("#user-res").click(function(){$("#user-form").show();$(".dialog-screen").show()});$("#user-error").click(function(){$("#user-form-error").show();$(".dialog-screen").show()});$("#user-info").click(function(){$("#user-form-info").show();$(".dialog-screen").show()});$(".dialog-screen").click(function(){$("#user-form").hide();$("#user-form-info").hide();$("#user-form-error").hide();$(this).hide()});$(".dialog-close").click(function(){$("#user-form").hide();$("#user-form-info").hide();$("#user-form-error").hide();$(".dialog-screen").hide()})});
+$(document).ready(function(){
+  $("#user-res").click(function(){
+    $("#user-form").show();
+    $(".dialog-screen").show()});
+    $("#user-error").click(function(){
+      $("#user-form-error").show();
+      $(".dialog-screen").show();
+    });
+    $("#user-info").click(function(){
+      $("#user-form-info").show();
+      $(".dialog-screen").show()
+    });
+    $(".dialog-screen").click(function(){
+      $("#user-form").hide();
+      $("#user-form-info").hide();
+      $("#user-form-error").hide();
+      $(this).hide()
+    });
+    $(".dialog-close").click(function(){
+      $("#user-form").hide();
+      $("#user-form-info").hide();
+      $("#user-form-error").hide();
+      $(".dialog-screen").hide()
+    });
+});
