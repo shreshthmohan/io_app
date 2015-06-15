9e5e14a4e65e2540f977d13ffb0b5bc4ffbc988e
Shreshth <sremog@gmail.com>
2015-06-15 07:01:45 +0000

    Done with visual dynamic features for activity page
    still need to work on the look

--- public/js/activity_pop.js
+++ public/js/activity_pop.js
@@ -2,29 +2,55 @@ $(document).ready(function(){
   $("a[id^='event-info-']").click(function(){
     var num = $(this).attr("id");
     var num_ex = num.split("-")[2];
-    console.log(num);
-    console.log(num_ex);
     $("#details" + num_ex).show();
+    $('.event-tab').addClass("active").siblings().removeClass("active")
     $(".events-info").addClass("active").siblings().removeClass("active");
     $(".dialog-screen").show()});
-  $("#retailer-info-1").click(function(){
-    $("#details1").show();
+  $("a[id^='retailer-info-']").click(function(){
+    var num = $(this).attr("id");
+    var num_ex = num.split("-")[2];
+    $("#details" + num_ex).show();
+    $('.gear-tab').addClass("active").siblings().removeClass("active")
     $(".retailers-info").addClass("active").siblings().removeClass("active");
     $(".dialog-screen").show()});
-  $("#group-info-1").click(function(){
-    $("#details1").show();
+  $("a[id^='group-info-']").click(function(){
+    var num = $(this).attr("id");
+    var num_ex = num.split("-")[2];
+    $("#details" + num_ex).show();
+    $('.group-tab').addClass("active").siblings().removeClass("active")
     $(".groups-info").addClass("active").siblings().removeClass("active");
     $(".dialog-screen").show()});
-  $("#school-info-1").click(function(){
-    $("#details1").show();
+  $("a[id^='school-info-']").click(function(){
+    var num = $(this).attr("id");
+    var num_ex = num.split("-")[2];
+    $("#details" + num_ex).show();
+    $('.learn-tab').addClass("active").siblings().removeClass("active")
     $(".schools-info").addClass("active").siblings().removeClass("active");
     $(".dialog-screen").show()});
   $(".dialog-screen").click(function(){
-    $("#details1").hide();
+    $("div[id^='details']").hide();
     $(this).hide()
   });
   $(".dialog-close").click(function(){
-    $("#details1").hide();
+    $("div[id^='details']").hide();
     $(".dialog-screen").hide()
   });
+
+  // tab head, click to brighten/activate
+  $('.event-tab').click(function() {
+    $(this).addClass("active").siblings().removeClass("active")
+    $('.events-info').addClass("active").siblings().removeClass("active");
+  })
+  $('.gear-tab').click(function() {
+    $(this).addClass("active").siblings().removeClass("active")
+    $('.schools-info').addClass("active").siblings().removeClass("active");
+  })
+  $('.learn-tab').click(function() {
+    $(this).addClass("active").siblings().removeClass("active")
+    $('.schools-info').addClass("active").siblings().removeClass("active");
+  })
+  $('.group-tab').click(function() {
+    $(this).addClass("active").siblings().removeClass("active")
+    $('.groups-info').addClass("active").siblings().removeClass("active");
+  })
 });
